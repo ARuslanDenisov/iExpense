9751dee573c75eaa107d7e7be0bb6faf5f7f79dd
ARuslanDenisov <angess47@gmail.com>
2024-06-08 17:52:09 +0700

    challenge done

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Руслан on 08.06.2024.
+//
+
+import Foundation
---
+++ iExpense/ListElement.swift
@@ -0,0 +1,18 @@
+//
+//  ListElement.swift
+//  iExpense
+//
+//  Created by Руслан on 08.06.2024.
+//
+
+import SwiftUI
+
+struct ListElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListElement()
+}
